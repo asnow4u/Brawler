1144baeddccecac9ab05ce0b33235c278633d631
mckencod <mckencod@oregonstate.edu>
2022-01-08 19:02:37 -0800

    Bunch of assets and some materilas and particles.

---
+++ Assets/Scripts/Items/Gun.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : Item, IItemInterface
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        base.Update();
+    }
+
+
+    public void UseItem()
+    {
+        Debug.Log("ACTIVATED");
+        Debug.Log(gameObject);
+    }
+}
