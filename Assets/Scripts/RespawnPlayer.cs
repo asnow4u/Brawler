8733cbbc17fa8db63a45b24ba838fd1d33e2ed1b
Joey Gasperino <joseph.gasperino.6471@mail.linnbenton.edu>
2022-02-02 20:38:16 -0800

    It respawns again! Horay! Thank you!

--- Assets/Scripts/RespawnPlayer.cs
+++ Assets/Scripts/RespawnPlayer.cs
@@ -2,34 +2,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class RespawnPlayer : MonoBehaviour
+public class RespawnPlayer : NetworkBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private Transform respawnPoint;
 
 
     // Start is called before the first frame update
-    void Start()
+    void Start()    
     {
         
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
-        {
-            player.transform.position = respawnPoint.transform.position;
-            Physics.SyncTransforms();
-            Rigidbody rb = other.GetComponent<Rigidbody>();
-            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
-        }
+        //if (IsServer)
+        //{
+            if (other.gameObject.tag == "Player")
+            {
+                other.transform.position = respawnPoint.transform.position;
+                //player.transform.position = respawnPoint.transform.position;
+                Physics.SyncTransforms();
+                Rigidbody rb = other.GetComponent<Rigidbody>();
+                rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+                Debug.Log("That's the player, uh huh!");
+            }
 
-        if (other.CompareTag("Item"))
-        {
-            Destroy(other.gameObject);
-        }
-
+            if (other.CompareTag("Item"))
+            {
+                Destroy(other.gameObject);
+            }
+        //}
     }
 
 }
